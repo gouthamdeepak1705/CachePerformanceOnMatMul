06c541d2cc644d03fe928f6dcb91690132681c41
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-16 13:06:32 -0400

    Update scatter.cpp

--- src/scatter.cpp
+++ src/scatter.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cstdlib>  // For rand() and srand()
 
 using namespace std;
 
@@ -17,63 +18,61 @@ void parsec_roi_end()
 
 }
 
-
 struct Result {
-	vector< vector<int> > A;
+    vector< vector<int> > A;
 };
 
 Result read(string filename) {
-	vector< vector<int> > A;
-	Result ab;
-	string line;
-	ifstream infile;
-	infile.open (filename.c_str());
+    vector< vector<int> > A;
+    Result ab;
+    string line;
+    ifstream infile;
+    infile.open(filename.c_str());
 
-	int i = 0;
-	while (getline(infile, line) && !line.empty()) {
-		istringstream iss(line);
-		A.resize(A.size() + 1);
-		int a, j = 0;
-		while (iss >> a) {
-			A[i].push_back(a);
-			j++;
-		}
-		i++;
-	}
+    int i = 0;
+    while (getline(infile, line) && !line.empty()) {
+        istringstream iss(line);
+        A.resize(A.size() + 1);
+        int a, j = 0;
+        while (iss >> a) {
+            A[i].push_back(a);
+            j++;
+        }
+        i++;
+    }
 
-	infile.close();
-	ab.A = A;
-	return ab;
+    infile.close();
+    ab.A = A;
+    return ab;
 }
 
-vector< vector<int> > ijkalgorithm(vector< vector<int> > A) {
-	int n = A.size();
+vector< vector<int> > scatter(vector< vector<int> > A) {
+    int n = A.size();
 
-	// initialise C with 0s
-	vector<int> tmp(n, 0);
-	vector< vector<int> > C(n, tmp);
+    // Generate 1,000 random indices and 1,000 random values
+    for (int i = 0; i < 1000; i++) {
+        int row = rand() % n;   // Random row index
+        int col = rand() % n;   // Random column index
+        int value = rand();     // Random value without bounds
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < n; k++) {
-				C[i][j] += A[i][k] * B[k][j];
-			}
-		}
-	}
-	return C;
+        // Scatter the random value into the matrix
+        A[row][col] = value;
+    }
+
+    return A;
 }
 
-int main (int argc, char* argv[]) {
-  srand(time(0));
-	string filename;
-	if (argc < 3) {
-		filename = "2000.in";
-	} else {
-		filename = argv[2];
-	}
-	Result result = read (filename);
+int main(int argc, char* argv[]) {
+    srand(time(0));
+    string filename;
+    if (argc < 3) {
+        filename = "2000.in";
+    } else {
+        filename = argv[2];
+    }
+    Result result = read(filename);
     parsec_roi_begin();
-	vector< vector<int> > C = ijkalgorithm(result.A);
+    vector< vector<int> > C = scatter(result.A);
     parsec_roi_end();
-	return 0;
+    return 0;
 }
