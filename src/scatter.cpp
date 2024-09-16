199a46a319d2915e939932d5759a166dd747b753
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-16 12:58:27 -0400

    Update scatter.cpp

--- src/scatter.cpp
+++ src/scatter.cpp
@@ -1,1 +1,79 @@
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ctime>
 
+using namespace std;
+
+void parsec_roi_begin() 
+{
+
+}
+
+void parsec_roi_end() 
+{
+
+}
+
+
+struct Result {
+	vector< vector<int> > A;
+};
+
+Result read(string filename) {
+	vector< vector<int> > A;
+	Result ab;
+	string line;
+	ifstream infile;
+	infile.open (filename.c_str());
+
+	int i = 0;
+	while (getline(infile, line) && !line.empty()) {
+		istringstream iss(line);
+		A.resize(A.size() + 1);
+		int a, j = 0;
+		while (iss >> a) {
+			A[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
+
+	infile.close();
+	ab.A = A;
+	return ab;
+}
+
+vector< vector<int> > ijkalgorithm(vector< vector<int> > A) {
+	int n = A.size();
+
+	// initialise C with 0s
+	vector<int> tmp(n, 0);
+	vector< vector<int> > C(n, tmp);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	return C;
+}
+
+int main (int argc, char* argv[]) {
+  srand(time(0));
+	string filename;
+	if (argc < 3) {
+		filename = "2000.in";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
+    parsec_roi_begin();
+	vector< vector<int> > C = ijkalgorithm(result.A);
+    parsec_roi_end();
+	return 0;
+}
