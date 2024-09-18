42cfcb528202fb689252e96f4cf4bf7b0f7c521c
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-18 00:22:15 -0400

    Update gather.cpp

--- src/gather.cpp
+++ src/gather.cpp
@@ -3,80 +3,91 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <ctime>
-#include <cstdlib>  
 
 using namespace std;
 
 void parsec_roi_begin() 
 {
-    // Start of Region of Interest
+
 }
 
 void parsec_roi_end() 
 {
-    // End of Region of Interest
+
 }
 
+
 struct Result {
-    vector< vector<int> > A;
+	vector< vector<int> > A;
 };
 
 Result read(string filename) {
-    vector< vector<int> > A;
-    Result ab;
-    string line;
-    ifstream infile;
-    infile.open(filename.c_str());
+	vector< vector<int> > A;
+	Result ab;
+	string line;
+	ifstream infile;
+	infile.open (filename.c_str());
 
-    int i = 0;
-    while (getline(infile, line) && !line.empty()) {
-        istringstream iss(line);
-        A.resize(A.size() + 1);
-        int a, j = 0;
-        while (iss >> a) {
-            A[i].push_back(a);
-            j++;
-        }
-        i++;
-    }
+	int i = 0;
+	while (getline(infile, line) && !line.empty()) {
+		istringstream iss(line);
+		A.resize(A.size() + 1);
+		int a, j = 0;
+		while (iss >> a) 
+        {
+			A[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
 
-    infile.close();
-    ab.A = A;
-    return ab;
+	infile.close();
+	ab.A = A;
+	return ab;
 }
 
-vector<int> gather(const vector< vector<int> >& A) {
+vector< vector<int> > scatter(vector< vector<int> > A) 
+{
     int n = A.size();
-    vector<int> gathered_values; 
+    vector<int > B(1000);
 
+    // Generate 1,000 random indices and 1,000 random values
     for (int i = 0; i < 1000; i++) {
-        int row = rand() % n;   
-        int col = rand() % n;  
+        int row = rand() % n;   // Random row index
+        int col = rand() % n;   // Random column index
 
-        gathered_values.push_back(A[row][col]);
+        // Scatter the random value into the matrix
+        B[i] = A[row][col];
     }
-
-    return gathered_values;
+    return A;
 }
 
-int main(int argc, char* argv[]) {
-    srand(time(0)); 
-    string filename;
-    if (argc < 3) {
-        filename = "2000.in";
-    } else {
-        filename = argv[2];
-    }
-    Result result = read(filename);
+
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+	string filename;
+	if (argc < 3) {
+		filename = "2000.in";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
     parsec_roi_begin();
-    vector<int> gathered = gather(result.A);  
+	vector< vector<int> > C = scatter(result.A);
     parsec_roi_end();
-    
-    for (int value : gathered) {
-        cout << value << " ";
-    }
-    cout << endl;
-    
-    return 0;
+	//printMatrix(C);
+	return 0;
 }
