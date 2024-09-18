ba5f8f617ce6e7d098280a9d56f92e742e98c5df
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-18 04:18:44 -0400

    Update convolve.cpp

--- src/convolve.cpp
+++ src/convolve.cpp
@@ -16,76 +16,65 @@ void parsec_roi_end()
 
 }
 
-
-struct Result {
+vector< vector<int> > read(string filename) {
 	vector< vector<int> > A;
-};
-
-Result read(string filename) {
-	vector< vector<int> > A;
-	Result ab;
 	string line;
 	ifstream infile;
-	infile.open (filename.c_str());
+	infile.open(filename.c_str());
 
 	int i = 0;
-	while (getline(infile, line) && !line.empty()) {
+	while (getline(infile, line)) {
 		istringstream iss(line);
 		A.resize(A.size() + 1);
-		int a, j = 0;
-		while (iss >> a) 
-        {
+		int a;
+		while (iss >> a) {
 			A[i].push_back(a);
-			j++;
 		}
 		i++;
 	}
 
 	infile.close();
-	ab.A = A;
-	return ab;
+	return A;
 }
 
-vector<vector<int>> initializeIdentityKernel(int size) {
-    vector<vector<int>> kernel(size, vector<int>(size, 0));
-    for (int i = 0; i < size; ++i) {
-        kernel[i][i] = 1; 
-    }
-    return kernel;
+// Perform 2D convolution with a 3x3 kernel
+vector< vector<int> > convolution2D(vector< vector<int> > A, vector< vector<float> > kernel) {
+	int n = A.size();
+	int m = A[0].size();
+
+	// Initialize output matrix C with the same size as A
+	vector<int> tmp(m, 0);
+	vector< vector<int> > C(n, tmp);
+
+	int kernelSize = kernel.size();
+	int offset = kernelSize / 2;
+
+	// Perform convolution
+	for (int i = offset; i < n - offset; i++) {
+		for (int j = offset; j < m - offset; j++) {
+			float sum = 0.0;
+			// Convolution operation
+			for (int ki = 0; ki < kernelSize; ki++) {
+				for (int kj = 0; kj < kernelSize; kj++) {
+					int ni = i + (ki - offset); // Neighbor index for matrix A
+					int nj = j + (kj - offset); // Neighbor index for matrix A
+					sum += A[ni][nj] * kernel[ki][kj];
+				}
+			}
+			C[i][j] = static_cast<int>(sum); // Store the result in C
+		}
+	}
+
+	return C;
 }
-
-    vector<vector<int>> convolve(const vector<vector<int>>& matrix, const vector<vector<int>>& kernel) {
-{
-    int n = matrix.size();
-    int k = kernel.size();
-    int outputSize = (n - k) + 1;
-    
-    vector<vector<int>> output(outputSize, vector<int>(outputSize, 0));
-
-    for (int i = 0; i <= n - k; i += 1) {
-        for (int j = 0; j <= n - k; j += 1) {
-            int sum = 0;
-            for (int ki = 0; ki < k; ++ki) {
-                for (int kj = 0; kj < k; ++kj) {
-                    sum += matrix[i + ki][j + kj] * kernel[ki][kj];
-                }
-            }
-            output[i ][j ] = sum;
-        }
-    }
-
-    return output;
-}
-}
-
 
 void printMatrix(vector< vector<int> > matrix) {
 	vector< vector<int> >::iterator it;
 	vector<int>::iterator inner;
-	for (it=matrix.begin(); it != matrix.end(); it++) {
+	for (it = matrix.begin(); it != matrix.end(); it++) {
 		for (inner = it->begin(); inner != it->end(); inner++) {
 			cout << *inner;
-			if(inner+1 != it->end()) {
+			if (inner + 1 != it->end()) {
 				cout << "\t";
 			}
 		}
@@ -93,18 +82,31 @@ void printMatrix(vector< vector<int> > matrix) {
 	}
 }
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 	string filename;
 	if (argc < 3) {
 		filename = "2000.in";
 	} else {
 		filename = argv[2];
 	}
-	Result result = read (filename);
-    vector<vector<int>> kernel = initializeIdentityKernel(7); //used identity matrix for 7X7
-    parsec_roi_begin();
-	vector< vector<int> > C = convolve(result.A,kernel);
-    parsec_roi_end();
-	//printMatrix(C);
+
+	// Read matrix A from file
+	vector< vector<int> > A = read(filename);
+
+	// Define a 3x3 Gaussian kernel
+	vector< vector<float> > gaussianKernel = {
+		{1 / 16.0, 2 / 16.0, 1 / 16.0},
+		{2 / 16.0, 4 / 16.0, 2 / 16.0},
+		{1 / 16.0, 2 / 16.0, 1 / 16.0}
+	};
+
+	// Perform convolution on matrix A
+	parsec_roi_begin();
+	vector< vector<int> > C = convolution2D(A, gaussianKernel);
+	parsec_roi_end();
+
+	// Uncomment the following line to print the result
+	// printMatrix(C);
+
 	return 0;
 }
