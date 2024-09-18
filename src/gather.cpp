5cf536828390a6eee825bf0f30e26b8d0fbe5a8b
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-18 02:02:39 -0400

    Update gather.cpp

--- src/gather.cpp
+++ src/gather.cpp
@@ -1,101 +1,93 @@
-// Updated code with additional checks
-
 #include <sstream>
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 using namespace std;
 
-void parsec_roi_begin() {}
+void parsec_roi_begin() 
+{
 
-void parsec_roi_end() {}
+}
+
+void parsec_roi_end() 
+{
+
+}
+
 
 struct Result {
-    vector< vector<int> > A;
+	vector< vector<int> > A;
 };
 
 Result read(string filename) {
-    vector< vector<int> > A;
-    Result ab;
-    string line;
-    ifstream infile;
-    infile.open(filename.c_str());
+	vector< vector<int> > A;
+	Result ab;
+	string line;
+	ifstream infile;
+	infile.open (filename.c_str());
 
-    if (!infile.is_open()) { // Check if file opened successfully
-        cerr << "Error opening file: " << filename << endl;
-        return ab; // Return empty Result
-    }
+	int i = 0;
+	while (getline(infile, line) && !line.empty()) {
+		istringstream iss(line);
+		A.resize(A.size() + 1);
+		int a, j = 0;
+		while (iss >> a) 
+        {
+			A[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
 
-    int i = 0;
-    while (getline(infile, line) && !line.empty()) {
-        istringstream iss(line);
-        A.resize(A.size() + 1);
-        int a, j = 0;
-        while (iss >> a) {
-            A[i].push_back(a);
-            j++;
-        }
-        i++;
-    }
-    infile.close();
-    ab.A = A;
-    return ab;
+	infile.close();
+	ab.A = A;
+	return ab;
 }
 
-vector<int> gather(const vector<vector<int>>& A) {
+vector< vector<int> > gather(vector< vector<int> > A) 
+{
     int n = A.size();
-    if (n == 0) {
-        cerr << "Error: Matrix is empty!" << endl;
-        return {};
+    vector<int > B(1000);
+
+    // Generate 1,000 random indices and 1,000 random values
+    for (int i = 0; i < 1000; i++) {
+        int row = rand() % n;   // Random row index
+        int col = rand() % n;   // Random column index
+
+        // Scatter the random value into the matrix
+        B[i] = A[row][col];
     }
-    
-    int number_of_indices = 1000;
-    vector<int> indices(number_of_indices), gathered_data(number_of_indices);
-    
-    // Generate 1,000 random indices
-    for(int i = 0; i < number_of_indices; i++) {
-        indices[i] = rand() % (n * n);  // Random index within the flattened matrix
-    }
-    
-    // Gather elements at the random indices
-    for(int i = 0; i < number_of_indices; i++) {
-        int row = indices[i] / n;  // Compute row
-        int col = indices[i] % n;  // Compute column
-        
-        if (row < 0 || row >= n || col < 0 || col >= A[row].size()) { // Check bounds
-            cerr << "Error: Index out of bounds at " << row << ", " << col << endl;
-            continue;
-        }
-        
-        gathered_data[i] = A[row][col];  // Gather the element
-    }
-    
-    return gathered_data;  // Return the gathered data
+    return A;
+}
+
+
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
 }
 
 int main (int argc, char* argv[]) {
-    srand(time(0));
-    string filename;
-    if (argc < 3) {
-        filename = "2000.in";
-    } else {
-        filename = argv[2];
-    }
-    Result result = read (filename);
-    if (result.A.empty()) { // Check if matrix read is successful
-        cerr << "Error: Matrix read unsuccessful or empty!" << endl;
-        return 1;
-    }
-
+	string filename;
+	if (argc < 3) {
+		filename = "2000.in";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
     parsec_roi_begin();
-    vector<int> C = gather(result.A);  // Corrected to vector<int>
+	vector< vector<int> > C = gather(result.A);
     parsec_roi_end();
-    
-    // Optionally print the gathered data as a 1D vector
-    //printVector(C);
-    
-    return 0;
+	//printMatrix(C);
+	return 0;
 }
