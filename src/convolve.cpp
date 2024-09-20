33ad09e66830bc48a1cfa134d08dcdd3f9a1409a
gouthamdeepak1705 <gfd5210@psu.edu>
2024-09-19 23:29:43 -0400

    Update convolve.cpp

--- src/convolve.cpp
+++ src/convolve.cpp
@@ -8,14 +8,11 @@ using namespace std;
 
 void parsec_roi_begin() 
 {
-
 }
 
 void parsec_roi_end() 
 {
-
 }
-
 
 struct Result {
 	vector< vector<int> > A;
@@ -26,21 +23,19 @@ Result read(string filename) {
 	Result ab;
 	string line;
 	ifstream infile;
-	infile.open (filename.c_str());
+	infile.open(filename.c_str());
 
 	int i = 0;
 	while (getline(infile, line) && !line.empty()) {
 		istringstream iss(line);
 		A.resize(A.size() + 1);
 		int a, j = 0;
-		while (iss >> a) 
-        {
+		while (iss >> a) {
 			A[i].push_back(a);
 			j++;
 		}
 		i++;
 	}
-
 	infile.close();
 	ab.A = A;
 	return ab;
@@ -54,38 +49,35 @@ vector<vector<int>> initializeIdentityKernel(int size) {
     return kernel;
 }
 
-    vector<vector<int>> convolve(const vector<vector<int>>& matrix, const vector<vector<int>>& kernel) {
-{
-    int n = matrix.size();
-    int k = kernel.size();
-    int outputSize = (n - k) + 1;
+vector<vector<int>> convolve(const vector<vector<int>>& matrix, const vector<vector<int>>& kernel) {
+    int matrixSize = matrix.size();
+    int kernelSize = kernel.size();
+    int outputSize = matrixSize - kernelSize + 1;
     
-    vector<vector<int>> output(outputSize, vector<int>(outputSize, 0));
+    vector<vector<int>> result(outputSize, vector<int>(outputSize, 0));
 
-    for (int i = 0; i <= n - k; i += 1) {
-        for (int j = 0; j <= n - k; j += 1) {
-            int sum = 0;
-            for (int ki = 0; ki < k; ++ki) {
-                for (int kj = 0; kj < k; ++kj) {
-                    sum += matrix[i + ki][j + kj] * kernel[ki][kj];
+    for (int row = 0; row <= matrixSize - kernelSize; ++row) {
+        for (int col = 0; col <= matrixSize - kernelSize; ++col) {
+            int convSum = 0;
+            for (int i = 0; i < kernelSize; ++i) {
+                for (int j = 0; j < kernelSize; ++j) {
+                    convSum += matrix[row + i][col + j] * kernel[i][j];
                 }
             }
-            output[i ][j ] = sum;
+            result[row][col] = convSum;
         }
     }
 
-    return output;
+    return result;
 }
-}
-
 
 void printMatrix(vector< vector<int> > matrix) {
 	vector< vector<int> >::iterator it;
 	vector<int>::iterator inner;
-	for (it=matrix.begin(); it != matrix.end(); it++) {
-		for (inner = it->begin(); inner != it->end(); inner++) {
+	for (it = matrix.begin(); it != matrix.end(); ++it) {
+		for (inner = it->begin(); inner != it->end(); ++inner) {
 			cout << *inner;
-			if(inner+1 != it->end()) {
+			if (inner + 1 != it->end()) {
 				cout << "\t";
 			}
 		}
@@ -93,18 +85,17 @@ void printMatrix(vector< vector<int> > matrix) {
 	}
 }
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 	string filename;
 	if (argc < 3) {
 		filename = "2000.in";
 	} else {
 		filename = argv[2];
 	}
-	Result result = read (filename);
-    vector<vector<int>> kernel = initializeIdentityKernel(7); //used identity matrix for 7X7
+	Result result = read(filename);
+    vector<vector<int>> kernel = initializeIdentityKernel(7); 
     parsec_roi_begin();
-	vector< vector<int> > C = convolve(result.A,kernel);
+	vector< vector<int> > C = convolve(result.A, kernel);
     parsec_roi_end();
-	//printMatrix(C);
 	return 0;
 }
